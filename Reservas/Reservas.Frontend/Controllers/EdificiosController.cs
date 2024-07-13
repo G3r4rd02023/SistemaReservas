161bec889ab8709a9cee80da8cbc1e1b1bf2c7cf
Gerardo Lanza <glanza007@gmail.com>
2024-07-12 21:58:34 -0600

    Crud edificios

--- Reservas/Reservas.Frontend/Controllers/EdificiosController.cs
+++ Reservas/Reservas.Frontend/Controllers/EdificiosController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Reservas.Frontend.Models;
+using Reservas.Shared.Data;
+using System.Text;
 
 namespace Reservas.Frontend.Controllers
 {
     public class EdificiosController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public EdificiosController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7009/");
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("/api/Edificios");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var edificios = JsonConvert.DeserializeObject<IEnumerable<Edificio>>(content);
+                return View("Index", edificios);
+            }
+            return View(new List<Edificio>());
         }
 
         public IActionResult Create()
@@ -14,14 +34,78 @@ namespace Reservas.Frontend.Controllers
             return View();
         }
 
-        public IActionResult Edit()
+        [HttpPost]
+        public async Task<IActionResult> Create(Edificio edificio)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(edificio);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("/api/Edificios", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["AlertMessage"] = "Edificio creado Exitosamente";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Ocurrio un error al crear el edificio";
+                }
+            }
+           
+            return View(edificio);
         }
 
-        public IActionResult Delete()
+        public async Task<IActionResult> Edit(int id)
         {
-            return View();
+            var response = await _httpClient.GetAsync($"/api/Edificios/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                TempData["ErrorMessage"] = "Error al obtener edificio";
+                return RedirectToAction("Index");
+            }
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var edificio = JsonConvert.DeserializeObject<Edificio>(jsonString);
+            
+            return View(edificio);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Edificio edificio)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(edificio);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PutAsync($"/api/Edificios/{id}", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["AlertMessage"] = "Edificio actualizada Exitosamente";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Ocurrio un error al actualizar el edificio";
+                    return RedirectToAction("Index");
+                }
+            }
+            
+            return View(edificio);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"/api/Edificios/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["AlertMessage"] = "Edificio eliminado Exitosamente";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Error al eliminar el edificio";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
